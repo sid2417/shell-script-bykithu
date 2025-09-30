4e48bb8af9268908125502ad39e23928825a3e5b
sid2417 <siddhartha.java9398@gmail.com>
2025-10-01 02:03:58 +0530

    nginx installation

--- 06-installation.sh
+++ 06-installation.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # using condition we install the nginx
+# we checking user is having root access or not
+
+USERNAME=$(id -u)
+
+if [ $USERNAME -eq 0 ]
+then
+    echo "You are a root user"
+else 
+    echo "Plese provide the root access to the user"
+fi
+
 
 dnf install nginx -y
