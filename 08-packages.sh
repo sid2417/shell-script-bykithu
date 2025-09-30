a62c54ad0f7df6f9bae78cc0ed6080ee915532a9
sid2417 <siddhartha.java9398@gmail.com>
2025-10-01 03:43:48 +0530

    nginx installation

--- 08-packages.sh
+++ 08-packages.sh
@@ -4,6 +4,16 @@ echo "Installing packages using for loop"
 echo "Number of vaiables in arguments $#"
 echo "All vaiables in arguments $@"
 
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then 
+    echo "Plese provide root access to the user.."
+    exit 2
+else
+    echo "user alredy having root access.."
+fi
+
+
 for packages in $@
 do
     echo $packages
@@ -17,14 +27,3 @@ do
     fi
 done
 
-# for packages in $@
-# do
-#     echo $packages
-#     dnf list installed $packages
-#     if [ $? -ne 0 ]
-#     then
-#         dnf install $packages -y
-#     else    
-#         echo "$packages is already installed in your server"
-#     fi
-# done
