b4e503c69a9534288ea6c5fc8b8f6c0143acac6d
sid2417 <siddhartha.java9398@gmail.com>
2025-10-03 01:48:52 +0530

    other script

--- 12-current-script.sh
+++ 12-current-script.sh
@@ -5,8 +5,22 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./13-other-script.sh
+# ./13-other-script.sh
 
-# source ./13-other-script.sh
+source ./13-other-script.sh
 
 echo "After calling other script, course: $COURSE"
+
+
+
+# ./13-other-script.sh 
+# Before calling other script, course: DevOps from Current Script
+# Process ID of current shell script: 1432
+# Variable value from Other script: DevOps from Other Script
+# Process ID of other script: 1433
+# After calling other script, course: DevOps from Current Script
+
+
+
+
+
