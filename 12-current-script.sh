a78fcb833c24745e67a445bfeb8a9e3e1da9abff
sid2417 <siddhartha.java9398@gmail.com>
2025-10-03 02:56:40 +0530

    other script

--- 12-current-script.sh
+++ 12-current-script.sh
@@ -12,8 +12,13 @@ source ./13-other-script.sh
 echo "After calling other script, course: $COURSE"
 
 
+# In two method while calling other scripts we required execution permissions of those other script files
 
-# ./13-other-script.sh 
+# 1st method::
+# ./13-other-script.sh (Calling other script)(variables are not changed and PID's different)
+# ( calling directly without using source two scripts are independent that not affect on each other it simply new shell will be created )
+
+
 # Before calling other script, course: DevOps from Current Script
 # Process ID of current shell script: 1432
 # Variable value from Other script: DevOps from Other Script
@@ -21,6 +26,14 @@ echo "After calling other script, course: $COURSE"
 # After calling other script, course: DevOps from Current Script
 
 
+# 2nd Method::
+# source ./13-other-script.sh (Calling other script with source)( variable will be override and PID's are same )
+# ( calling other script using source, in the current script variable will be override from the other script )
 
+# Before calling other script, course: DevOps from Current Script
+# Process ID of current shell script: 1474
+# Variable value from Other script: DevOps from Other Script
+# Process ID of other script: 1474
+# After calling other script, course: DevOps from Other Script
 
 
---
+++ 14-delete-old-files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Deleting the old files
+
+# Steps:
+# First find out the which type of files we need to delete and how many days back 
+# Before delete print the files to know those files are correct or not
+
+# To create old files ( touch -d 20240405 filename.log  )
+# To find that particular file is exist or not ( find . -name "*.sh" -mtime +14 )
+
+
+# Q: Delete the old files in /tmp directory with .log format morethan 30 days older
+
+SOURCE_PATH=$(/tmp/)
+
+if [ -d $SOURCE_PATH ]
+then
+    echo "your mentioned directory will be existed.."
+else
+    echo "you mentioned the path is wrong...Please check your path"
+    exit 4
+fi
+
+
+DELETE_FILES=$(find $SOURCE_PATH -name "*.log" -mtime +30)
+
+while IFS read -r checkline
+do 
+    echo "Deleting files : $checkline"
+    #rm -r $checkline
+done <<< $DELETE_FILES
+
+
