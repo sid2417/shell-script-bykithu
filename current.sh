5c25b901fc6d33e84209338b071043c07a7d8c99
sid2417 <siddhartha.java9398@gmail.com>
2025-10-04 16:24:24 +0530

    calling other script

--- current.sh
+++ current.sh
@@ -5,3 +5,9 @@ VARIABLE=current
 echo "this is current script :: $VARIABLE"
 
 echo "this is pid of current :: $$ "
+
+./current.sh
+
+echo "after calling current script :: $VARIABLE"
+
+echo "after calling pid of current :: $$ "
---
+++ other.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+VARIABLE=other
+
+echo "this is other script :: $VARIABLE"
+
+echo "this is pid of other script :: $$"
