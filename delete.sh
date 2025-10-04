1c8bad1357fbdba6e69e6d3ced4af27fefc37078
sid2417 <siddhartha.java9398@gmail.com>
2025-10-04 16:52:58 +0530

    calling other script

--- delete.sh
+++ delete.sh
@@ -5,12 +5,12 @@
 
 FILELIST="/tmp/abcd/"
 
-DELETELIST=$(find $FILELIST -name "*.log" -mtime +14)
+DELETELIST=$(find $FILELIST -name "*.sh" -mtime +14)
 
 # DELETE_FILES=$(find $SOURCE_PATH -name "*.sh" -mtime +1)
 
 while IFS= read -r line
 do 
     echo "printing the deleting files :  $line" 
-    rm -r $line
+    #rm -r $line
 done <<< $DELETELIST
