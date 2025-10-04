3a15bac108a7f8c416cdfc77074ec527bd9e28cc
sid2417 <siddhartha.java9398@gmail.com>
2025-10-04 15:49:34 +0530

    calling other script

--- todaysample.sh
+++ todaysample.sh
@@ -2,13 +2,13 @@
 
 ### now we are calling the other scripts
 
-VARIABLE=CURRENT
+VARIABLE=TOday
 
-echo "before calling the is a current script"
+echo "before calling the is a TOmorrow script $VARIABLE"
 echo "this is value of pid $$ "
 
 ./tomorrowsample.sh
 
-echo "after calling the Tomorrow script"
+echo "after calling the Tomorrow script $VARIABLE"
 echo "this is pid of after calling :: $$"
 
--- tomorrowsample.sh
+++ tomorrowsample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 VARIABLE=Tomorrow
-echo "This is Tomorrow "
+echo "This is Tomorrow $VARIABLE "
 echo "this is pid of :: $$"
