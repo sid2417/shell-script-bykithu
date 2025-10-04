e7a70a2e59d5c5b1388ecd0629f23064171dad0e
sid2417 <siddhartha.java9398@gmail.com>
2025-10-04 16:15:31 +0530

    calling other script

--- tomorrowsample.sh
+++ tomorrowsample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VARIABLE=tomorrow
-echo "this is the value of tomorrow script :: $VARIABLE "
-echo "this is the pid of tomorrow script :: $$"
+# VARIABLE=tomorrow
+# echo "this is the value of tomorrow script :: $VARIABLE "
+# echo "this is the pid of tomorrow script :: $$"
