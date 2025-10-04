bc15abc47af0e121bafdd35240fb2a92a05475e8
sid2417 <siddhartha.java9398@gmail.com>
2025-10-04 16:06:41 +0530

    calling other script

--- todaysample.sh
+++ todaysample.sh
@@ -7,10 +7,19 @@ VARIABLE=today
 echo "before calling the script name is today :: $VARIABLE"
 echo "this is pid of today script :: $$ "
 
-./tomorrowsample.sh
+source ./tomorrowsample.sh
 
 echo "after calling the tomorrow script the variable of today :: $VARIABLE"
 echo "after calling the tomorrow script the pid of today :: $$"
+
+
+# before calling the is a TOmorrow script TOday
+# this is value of pid 1592
+# This is Tomorrow Tomorrow
+# this is pid of :: 1593
+# after calling the Tomorrow script TOday
+# this is pid of after calling :: 1592
+
 
 
 
